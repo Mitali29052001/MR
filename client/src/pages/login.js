794a49e03bc230b6261d31abf2198ebbe018ca91
mitali <mitalikadam29052001@gmail.com>
2023-08-03 06:08:07 -0700

    Login, Register

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,10 +1,41 @@
 import React from 'react'
-import Default from '../components/Default'
-function profile() {
+import {Row, Col, Form, Input, Button} from 'antd'
+import { Link } from 'react-router-dom'
+// eslint-disable-next-line no-unused-vars
+import './login.css'
+function Login() {
+    function login(values) {
+        console.log(values)
+    }
     return (
-       <div>
-        <h1>Login</h1>
+        <div>
+            <Row justify='center' className='login-1'>
+                <Col lg={10} xs={24}>
+                    <Form layout='vertical' className='bs1 p-3' onFinish={login}>
+                        <h3>Login</h3>
+                        <hr />
+                        <Form.Item label="Firstname" name="Firstname" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Lastname" name="Lastname" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Username" name="Username" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Email id" name="Email id" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Password" name="Password" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                       
+                        <Button htmlType='Submit'>Login</Button>
+                        <Link to ='/register'>Click here if not yet register</Link>
+            </Form>
+        </Col>
+       </Row>
        </div>
     )
 }
-export default profile
+export default Login
--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import {Row, Col, Form, Input, Button} from 'antd'
+import { Link } from 'react-router-dom'
 // eslint-disable-next-line no-unused-vars
-import Default from '../components/Default'
-function profile() {
+import './register.css'
+function Register() {
+    function register(values) {
+        console.log(values)
+    }
     return (
         <div>
-        <h1>Register</h1>
+            <Row justify='center' className='register-1'>
+                <Col lg={10} xs={24}>
+                    <Form layout='vertical' className='bs1 p-3' onFinish={register}>
+                        <h3>Register</h3>
+                        <hr />
+                        <Form.Item label="Firstname" name="Firstname" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Lastname" name="Lastname" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Username" name="Username" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Email id" name="Email id" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Password" name="Password" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Form.Item label="Confirm Password" name="Confirm Password" rules={[{ require: true }]}>
+                            <Input />
+                        </Form.Item>
+                        <Button htmlType='Submit'>Register</Button>
+                        <Link to ='/login'>Click here if already register</Link>
+            </Form>
+        </Col>
+       </Row>
        </div>
     )
 }
-export default profile
+export default Register
