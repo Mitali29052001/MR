216da5bba8fc3d22122a399edfe8ad81cf00312f
riddhizavare <124781722+riddhizavare@users.noreply.github.com>
2023-08-10 12:23:53 +0530

    Update home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,8 +5,6 @@ import { Col, Row } from 'antd';
 import Post from '../components/post';
 
 function Home() {
-  // Use the correct state name from your rootReducer
-  // eslint-disable-next-line no-unused-vars
   const {users} = useSelector(state => state.usersReducer)
   const {posts} = useSelector(state => state.postsReducer)
   return (
